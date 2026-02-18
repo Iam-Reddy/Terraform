6932a0d32b2ea743dcec1bd11f1a5b60dd3c8ca1
schejerla <schejerla@rivier.edu>
2026-02-18 16:47:03 -0600

    changes

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_storage_account" "walmart" {
   resource_group_name      = azurerm_resource_group.walmart.name
   location                 = azurerm_resource_group.walmart.location
   account_tier             = "Standard"
-  account_replication_type = "LOCALLY_REDUNDANT"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "dev"
