2ff59f3148e2609726c01f95690fc8fa705c67d2
schejerla <schejerla@rivier.edu>
2026-02-18 17:54:50 -0600

    changes

--- main.tf
+++ main.tf
@@ -11,6 +11,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "walmart-tfstate"
+    storage_account_name = "walmarttfstate${random_integer.rand.result}"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -28,7 +35,7 @@ resource "azurerm_resource_group" "walmart" {
 }
 
 resource "azurerm_storage_account" "walmarttfstate" {
-  name                     = "walmarttf${random_integer.rand.result}"
+  name                     = "walmarttfstate${random_integer.rand.result}"
   resource_group_name      = azurerm_resource_group.walmart.name
   location                 = azurerm_resource_group.walmart.location
   account_tier             = "Standard"
@@ -40,8 +47,7 @@ resource "azurerm_storage_account" "walmarttfstate" {
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  name               = "tfstate"
-  storage_account_id = azurerm_storage_account.walmarttfstate.id
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.walmarttfstate.id
   container_access_type = "private"
 }
-
